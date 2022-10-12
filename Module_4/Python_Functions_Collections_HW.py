25794bcbe6033d7b878087c613b1c31f1ace3ac5
natalelea <74679548+natalelea@users.noreply.github.com>
2022-10-12 22:08:30 +0300

    Updated module 4 HW

--- Module_4/Python_Functions_Collections_HW.py
+++ Module_4/Python_Functions_Collections_HW.py
@@ -1,76 +1,94 @@
 # Function for creating a list of random number of dicts
-def dict_list_creation():
+def create_list_of_dicts():
     import string
     import random
     result = []
     for i in range(random.randint(2, 10)):
-        size = random.randint(1, 26)
-        keys = random.sample(string.ascii_lowercase, size)
-        values = (random.randint(1, 100) for j in range(size))
+        dict_size = random.randint(1, 26)
+        keys = random.sample(string.ascii_lowercase, dict_size)
+        values = (random.randint(1, 100) for j in range(dict_size))
         one_dict = dict(zip(keys, values))
         result.append(one_dict)
     return result
 
 
 # Function for creating one common dict
-def dicts_union():
-    dict_number = 0
+def union_dicts_to_one_dict(all_dicts):
+    current_dict_number = 0
     cnt = 0
-    lst = []
     result = {}
-    all_dicts = dict_list_creation()
+    dicts_keys = []
+    for one_dict in all_dicts:
+        for dict_key in one_dict:
+            dicts_keys.append(dict_key)
 
     for one_dict in all_dicts:
-        for dict_value in one_dict:
-            lst.append(dict_value)
-
-    for one_dict in all_dicts:
-        dict_number += 1
+        current_dict_number += 1
 
         for dict_key in one_dict:
             dict_value = one_dict.get(dict_key)
             cnt = 0
 
-            for i in lst:
+            for i in dicts_keys:
                 if dict_key == i:
                     cnt += 1
+
             if cnt == 1:
                 result[dict_key] = dict_value
             elif cnt > 1:
-                dict_key_num = dict_key + '_' + str(dict_number)
+                dict_key_num = dict_key + '_' + str(current_dict_number)
                 dict_value_num = dict_value
 
                 result[dict_key_num] = dict_value_num
 
-                for k in range(1, dict_number):
-                    dict_key_seq = dict_key + '_' + str(k)
-
-                    if dict_key_seq in result:
-                        dict_value_seq = result[dict_key_seq]
-
-                        if dict_value_seq < dict_value_num:
-                            result.pop(dict_key_seq)
-                        elif dict_value_seq >= dict_value_num:
-                            result.pop(dict_key_num)
     return result
 
 
 # Function for ordering the dictionary by key values (in alphabetic order)
-def dict_ordering(dictionary):
+def order_dict_by_keys(dictionary):
     result = dict(sorted(dictionary.items()))
+    return result
+
+
+# Function for choosing max value for every alphabet letter
+def get_dict_of_letters_with_max_value(one_dict):
+    result = {}
+    cnt = 0
+    all_letters_from_dict = []
+
+    for dict_key in one_dict:
+        alphabet_letter = str(dict_key)[0]
+        if alphabet_letter not in all_letters_from_dict:
+            all_letters_from_dict.append(alphabet_letter)
+
+    for k in all_letters_from_dict:
+        max_dict_value = 0
+        max_dict_key = 0
+
+        for dict_key in one_dict:
+            if dict_key[0] == k:
+                if one_dict[dict_key] > max_dict_value:
+                    max_dict_key = dict_key
+                    max_dict_value = one_dict[dict_key]
+            elif dict_key == k:
+                result[dict_key] = one_dict[dict_key]
+
+        result[max_dict_key] = max_dict_value
+
     return result
 
 
 # PLEASE, NOTE:
 # For verifying results you may uncomment the lines below
 
-# random_dicts = dict_list_creation()
+# random_dicts = create_list_of_dicts()
 # print(random_dicts)
 
-# common_dict = dicts_union()
+# common_dict = union_dicts_to_one_dict(random_dicts)
 # print(common_dict)
 
-# common_dict = dicts_union()
-# ordered_common_dict = dict_ordering(common_dict)
-# print(common_dict)
-# print(ordered_common_dict)
+# ordered_dict = order_dict_by_keys(common_dict)
+# print(ordered_dict)
+
+# dict_without_duplicates = get_dict_of_letters_with_max_value(ordered_dict)
+# print(dict_without_duplicates)
--- Module_4/Python_Functions_Strings_HW.py
+++ Module_4/Python_Functions_Strings_HW.py
@@ -1,47 +1,45 @@
 # Function for fixing "iz"
-def grammar_replace(text):
-    result = text.replace(' iz ', ' is ')
+def replace_iz_with_is(text):
+    result = text.capitalize().replace(' iz ', ' is ')
     return result
 
 
 # Function for normalizing letter case in the text
-def capitalizing(text):
-    c = text.capitalize().split(' ')
+def capitalize_first_letter(text):
+    processed_text = text.capitalize().split(' ')
     cnt = 0
-    for i in range(0, len(c)):
-        if c[i] not in ['', ' ', '\n'] and cnt == 1:
-            c[i] = c[i].capitalize()
+    for i in range(0, len(processed_text)):
+        if processed_text[i] not in ['', ' ', '\n'] and cnt == 1:
+            processed_text[i] = processed_text[i].capitalize()
             cnt = 0
-        if c[i].endswith(':\n') or c[i].endswith('.') or c[i].endswith('\n'):
+        if processed_text[i].endswith(':\n') or processed_text[i].endswith('.') or processed_text[i].endswith('\n'):
             cnt = 1
-    return ' '.join(c)
+    return ' '.join(processed_text)
 
 
 # Function for creating sentence from the last words
-def sentence_creation(text):
-    text = grammar_replace(text)
-    c = text.capitalize().split(' ')
-    sent = ''
-    cnt = 0
-    for i in range(0, len(c)):
-        if c[i].endswith(':\n') or c[i].endswith('.') or c[i].endswith('\n'):
-            sent += c[i]
-            cnt = 1
-    sent = sent.replace('\n', '').replace('.', ' ').replace(':', ' ').replace('  ', ' ').strip() + '.'
-    return sent
+def get_last_word_of_sentences(text):
+    text = replace_iz_with_is(text)
+    processed_text = text.capitalize().split(' ')
+    result = ''
+    for i in range(0, len(processed_text)):
+        if processed_text[i].endswith(':\n') or processed_text[i].endswith('.') or processed_text[i].endswith('\n'):
+            result += processed_text[i]
+    result = result.replace('\n', '').replace('.', ' ').replace(':', ' ').replace('  ', ' ').strip() + '.'
+    return result
 
 
 # Function for executing fixing "iz", normalizing letter case and adding the new sentence to the end of text
-def text_union(text):
-    corrected_grammar = grammar_replace(text)
-    formatted_text = capitalizing(corrected_grammar)
-    sentence = sentence_creation(text)
-    result = formatted_text + ' ' + sentence
+def normalize_text(text):
+    grammarly_correct_text = replace_iz_with_is(text)
+    formatted_text = capitalize_first_letter(grammarly_correct_text)
+    joint_sentence = get_last_word_of_sentences(text)
+    result = formatted_text + ' ' + joint_sentence
     return result
 
 
 # Function for counting spaces in the text
-def space_count(text):
+def count_spaces_in_text(text):
     result = text.count(' ') + text.count('\n') + text.count('\t')
     return f'The number of whitespace characters in this text is {result}.'
 
@@ -61,17 +59,17 @@ input_text = 'homEwork:\n  ' \
 # PLEASE, NOTE:
 # For verifying results you may uncomment the lines below
 
-# corrected_text = grammar_replace(input_text)
+# corrected_text = replace_iz_with_is(input_text)
 # print(corrected_text)
 
-# capitalized_text = capitalizing(input_text)
+# capitalized_text = capitalize_first_letter(input_text)
 # print(capitalized_text)
 
-# test_sentence = sentence_creation(input_text)
+# test_sentence = get_last_word_of_sentences(input_text)
 # print(test_sentence)
 
-# formatted_output = text_union(input_text)
+# formatted_output = normalize_text(input_text)
 # print(formatted_output)
 
-# total_spaces = space_count(input_text)
+# total_spaces = count_spaces_in_text(input_text)
 # print(total_spaces)
